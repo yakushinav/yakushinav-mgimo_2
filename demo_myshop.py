4d6dd44ac12df835b32349614b133e3f11d34955
unknown <yakushin.aleksey@gmail.com>
2020-03-28 23:06:53 +0300

    2. Создание классов для БД 3. Тестовые данные 4. Данные для обучения 5. Бот

--- demo_myshop.py
+++ demo_myshop.py
@@ -1,7 +1,15 @@
 
 from my_shop import MyShop
+import pandas as pd
 
 shop=MyShop("myShop.xml")
 shop.print_materials()
 
+
+
+shop.addSampleData(200,3,4)
+
 shop.print_products()
+
+df=shop.getTrainingData()
+print(df)
--- my_shop.py
+++ my_shop.py
@@ -1,5 +1,16 @@
 #подключаем библиотеку
 import xml.dom.minidom as minidom
+import xml.etree.ElementTree as ET
+import random
+import pandas as pd
+
+def prettify(elem):
+    """Форматирование для получения читабельного XML.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent='\t')
+
 
 #Общий класс для хранения справочников material, season, color, address
 class Catalog:
@@ -69,7 +80,107 @@ class MyShop:
         for k in self.products:
             self.products[k].print_product(self.materials, self.seasons, self.colors, self.categories)
 
+    def addSampleData(self, nProd, nCustomer, nOrder):
+        maxProd = max(self.products) + 1
+        for i in range(nProd):
+            dom=minidom.Document()
+            elem=dom.createElement("item")
+            elements={}
+            elements["id"]=maxProd
+            elements["name"]="Товар_"+str(i)
+            elements["category_id"]=1 #категория товара (нужно будет выбрать случайно из категорий
+            elements["price"]=random.randint(100,1000)
+            elements["color_id"]=random.choice(list(self.colors))
+            elements["size"]=random.choice(["M","L","S","XL","XS"])
+            elements["season_id"]=random.choice(list(self.seasons))
+            elements["material_id"] = random.choice(list(self.materials.items))
 
+            for k in elements:
+                field=dom.createElement(k)
+                text = dom.createTextNode(str(elements[k]))
+                field.appendChild(text)
+                elem.appendChild(field)
+            dom.appendChild(elem)
+            self.products[int(elem.getElementsByTagName("id")[0].firstChild.data)] = Product(elem)
+            maxProd = maxProd + 1  # номер следующего товара
 
+    def getTrainingData(self):
+        # Поскольку сейчас у нас нет заказов (не реализован шаг 3), то добавим просто случайные данные
+        # если реализован этот шаг, то этот код нужно будет удалить
+        order = {}
+        for i in range(1500):
+            order[i + 1] = [i + 1, random.choice(list(self.products)), random.randint(1, 30)]
+        # Поскольку сейчас у нас нет клиентов (не реализован шаг 3), то добавим просто случайные данные
+        # если реализован этот шаг, то этот код нужно будет удалить
+        customer = {}
+        for i in range(30):
+            customer[i + 1] = [i + 1, random.randint(18, 65), random.choice([0, 1]),
+                               random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]
 
+        # Списки для хранения тренировочных данных
+        pName = []
+        pCategory = []
+        pPrice = []
+        pColor = []
+        pSeason = []
+        pMaterial = []
+        cAge = []
+        cSex = []
+        cAddress = []
 
+        # Проходим по всем заказам
+        for i in order:
+            # Выбираем из таблицы Товары товар по номеру из Заказа и добавляем поля в список
+            pName.append(self.products[order[i][1]].name)
+            pCategory.append(self.products[order[i][1]].category)
+            pPrice.append(self.products[order[i][1]].price)
+            pColor.append(self.products[order[i][1]].color)
+            pSeason.append(self.products[order[i][1]].season)
+            pMaterial.append(self.products[order[i][1]].material)
+            # Выбираем из таблицы Клиенты клиента по номеру из Заказа и добавляем поля в список
+            cAge.append(customer[order[i][2]][0])
+            cSex.append(customer[order[i][2]][1])
+            cAddress.append(customer[order[i][2]][2])
+        # Создаем фрейм данных
+        df = pd.DataFrame({'name': pName, 'category': pCategory, 'price': pPrice, 'color': pColor, 'season': pSeason,
+                           'material': pMaterial, 'age': cAge, 'sex': cSex, 'address': cAddress})
+        return df
+
+    def saveXML(self, filename):
+        # создаем корневой элемент
+        data = ET.Element('myShop')
+        # создаем таблицу Товары
+        products = ET.SubElement(data, 'products')
+        # Записываем в нее все товары
+        for c in self.products:
+            # Добавляем элемент Товар
+            prod = ET.SubElement(products, 'products')
+            # Добавляем поле к элементу Товар
+            id = ET.SubElement(prod, 'id')
+            id.text = str(c)
+            # Добавляем поле к элементу Товар
+            name = ET.SubElement(prod, 'name')
+            name.text = self.products[c].name
+            # Добавляем поле к элементу Товар
+            category = ET.SubElement(prod, 'category_id')
+            category.text = str(self.products[c].category)
+            # Добавляем поле к элементу Товар
+            price = ET.SubElement(prod, 'price')
+            price.text = str(self.products[c].price)
+            # Добавляем поле к элементу Товар
+            color = ET.SubElement(prod, 'color_id')
+            color.text = str(self.products[c].color)
+            # Добавляем поле к элементу Товар
+            size = ET.SubElement(prod, 'size')
+            size.text = self.products[c].size
+            # Добавляем поле к элементу Товар
+            season = ET.SubElement(prod, 'season_id')
+            season.text = str(self.products[c].season)
+            # Добавляем поле к элементу Товар
+            material = ET.SubElement(prod, 'material_id')
+            material.text = str(self.products[c].material)
+        # Преобразуем в читабельный XML
+        md = prettify(data)
+        # Записываем в файл
+        myfile = open(filename, "w", encoding='utf8')
+        myfile.write(md)
